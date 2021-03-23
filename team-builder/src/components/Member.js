2a11c698a555d7b5c82f3bfdd4910c8c6ae8d804
Daniel B Kim <danielkim@Daniels-MacBook-Pro.local>
2021-03-23 19:59:42 -0400

    adds final MVP submission

--- team-builder/src/components/Member.js
+++ team-builder/src/components/Member.js
@@ -3,9 +3,13 @@ import React from 'react';
 export default function Member(props) {
     // console.log(firstName, lastName, email);
     // console.log(props.firstName)
+    const { member } = props;
+
     return (
         <div className='member-container'>
-            <h3>This is the section for members</h3>
+            <h3>{ member.name }</h3>
+            <p>{ member.email }</p>
+            <p>{ member.role }</p>
         </div>
         
     );
