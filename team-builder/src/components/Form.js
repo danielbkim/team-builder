bc918779ac42292d17d7aeb107c9eccc40f499e8
Daniel B Kim <danielkim@Daniels-MacBook-Pro.local>
2021-03-23 19:30:32 -0400

    build change and submit handlers for form

--- team-builder/src/components/Form.js
+++ team-builder/src/components/Form.js
@@ -3,18 +3,31 @@ import { React, useState } from 'react';
 const initialFormValues = { name: "", email: "", role: "" };
 
 
+
 export default function Form(props) {
-    
+
     const [formValues, setFormValues] = useState(initialFormValues);
 
+    const change = (e) => {
+        const { name, value } = e.target;
+        // console.log(e.target);
+        // console.log(name, value);
+        setFormValues({...formValues, [name]: value });
+    };
+    
+    const submit = (e) => {
+        e.preventDefault();
+        
+    
+    };
     console.log(props);
     return (
-        <form>
-            <input name='name' type='text' placeholder='i.e. Tony' required></input>
+        <form onSubmit={submit}>
+            <input name='name' type='text' value={formValues.name} onChange={change} placeholder='i.e. Tony' required></input>
             <br></br>
-            <input name='email' type='text' placeholder='i.e. Stark' required></input>
+            <input name='email' type='text' value={formValues.email} onChange={change} placeholder='i.e. Stark' required></input>
             <br></br>
-            <input name='role' type='text' placeholder='i.e tonystark@ironman.com' required></input>
+            <input name='role' type='text' value={formValues.role} onChange={change} placeholder='i.e tonystark@ironman.com' required></input>
             <br></br>
             <button>submit</button>
         </form>
